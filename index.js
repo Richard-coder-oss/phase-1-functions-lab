b3f81c59443276b5548d73e14adec148c3c99d3f
richard miruka onsare <richardonsare15@gmail.com>
2023-03-14 23:44:56 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solution in this file!
+
+function distanceFromHqInBlocks(location) {
+    const hq = 42;
+    return Math.abs(location - hq);
+  }
+  
+  function distanceFromHqInFeet(location) {
+    const blockInFeet = 264; // 1 block is 264 feet
+    return distanceFromHqInBlocks(location) * blockInFeet;
+  }
+  
+  function distanceTravelledInFeet(start, end) {
+    const blockInFeet = 264; // 1 block is 264 feet
+    return Math.abs(start - end) * blockInFeet;
+  }
+  
